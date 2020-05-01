1499103dbd74dc2fd50b6d1c11d0694ee10b0ff7
Alec Taylor <alectaylor312@gmail.com>
2020-05-01 15:16:08 -0400

    Change method of playing music from web browser to playsound.

--- bebox.py
+++ bebox.py
@@ -3,21 +3,36 @@ import pafy
 import time
 import youtube_dl
 import webbrowser
-queue = []
-pafy.set_api_key("AIzaSyAy2195YHslXWtjZY-0W_SdmVscXVzYL2Y")
+from playsound import playsound
+from multiprocessing import Process
+link_queue = []
+song_queue = []
+process_queue = []
+counter = 0
+id_counter = 0
+finished_playthrough = False
 
-def addqueue(search):
+current_song = None
+
+
+def addlink_queue(search):
+    global counter, id_counter
     os.system('youtube-dl -j --dump-json "ytsearch1:{0}" > json.txt'.format(search))
     file = open('json.txt', "r")
     lines = file.readlines()
     tmpstr = lines[0]
     tmpid = tmpstr[8:19]
     link = "https://www.youtube.com/watch?v=" + tmpid
-    queue.append(link)
-    print(link + "has been added to queue")
-def play():
-    print("playing " + queue[0])
-    webbrowser.open(queue[0])
+    print(link + "has been added to link_queue")
+    song_name = str(id_counter) + '.mp3'
+    os.system('youtube-dl --extract-audio --audio-format mp3 '  + link + ' --output "' + str(id_counter) + '.%(ext)s"')
+    link_queue.append(link)
+    song_queue.append(song_name)
+    process_queue.append(Process(name="playsong", target=play_song))
+    id_counter += 1
+def play_song():
+    print("playing " + link_queue[0]) # Figure out how the fuck to play the next song in queue once the first is finished without manually skipping
+    playsound(song_queue[0])
 def isPlaying(link, tic):
     vid = pafy.new(link)
     toc = tic + vid.length
@@ -25,22 +40,15 @@ def isPlaying(link, tic):
         return True
     else:
         return False
+def skip():
+    process_queue[0].terminate()
+    os.remove(song_queue[0])
+    process_queue.pop(0)
+    song_queue.pop(0)
+    process_queue[0].start()
 
-first = False
-gotFirst = False
-tic = 0
+
 while True:
-    if gotFirst:
-        if first:
-            tic = int(time.perf_counter())
-            first = False
-            play()
-        elif len(queue) > 1 and isPlaying(queue[0], tic):
-            tic = int(time.perf_counter())
-            queue.pop(0)
-            play()
-        else:
-            print("song still playing")
     inp = input("Enter Song Request with !song:")
     req = inp[0:5]
     if req == "!song":
@@ -49,25 +57,24 @@ while True:
             print("must add song request after !song")
             continue
         else:
-            addqueue(search)
-            if not gotFirst:
-                first = True
-                gotFirst = True
+            addlink_queue(search)
+            if len(song_queue) == 1:
+                print("playing song now")
+                process_queue[0].start()
+
     if req == "!skip":
-        if len(queue) == 1:
-            queue.pop(0)
-            tic = int(time.perf_counter())
-            gotFirst = False
-            first = False
-        elif len(queue) > 1:
-            tic = int(time.perf_counter())
-            queue.pop(0)
-            play()
+        if len(song_queue) > 1:
+            skip()
         else:
-            print("nothing to skip")
+            print("No songs left in queue")
     if req == "!done":
-        break;
+        break
 
 
+for p in process_queue:
+    if p != None:
+        p.terminate()   
 
-
+for song in song_queue:
+    print(song)
+    os.remove(song)
