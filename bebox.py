82b5317453397d86801b1f30c8b70c2fa09bc993
bebouth98 <hunterbe1998@gmail.com>
2020-05-01 00:28:11 -0400

    initial commit

---
+++ bebox.py
@@ -0,0 +1,56 @@
+import os
+import pafy
+import time
+import youtube_dl
+import webbrowser
+queue = []
+pafy.set_api_key("AIzaSyAy2195YHslXWtjZY-0W_SdmVscXVzYL2Y")
+
+def addqueu(search):
+    os.system('youtube-dl -j --dump-json "ytsearch1:{0}" > json.txt'.format(search))
+    file = open('json.txt', "r")
+    lines = file.readlines()
+    tmpstr = lines[0]
+    tmpid = tmpstr[8:19]
+    link = "https://www.youtube.com/watch?v=" + tmpid
+    queue.append(link)
+    print(link + "has been added to queue")
+
+def isPlaying(link, tic):
+    vid = pafy.new(link)
+    toc = tic + vid.length
+    if int(time.perf_counter()) >= toc:
+        return True
+    else:
+        return False
+
+first = False
+gotFirst = False
+tic = 0
+while True:
+    if gotFirst:
+        if first:
+            tic = int(time.perf_counter())
+            first = False
+            print("playing " + queue[0])
+            webbrowser.open(queue[0])
+        elif len(queue) > 1 and isPlaying(queue[0], tic):
+            tic = int(time.perf_counter())
+            queue.pop(0)
+            print("playing" + queue[0])
+            webbrowser.open(queue[0])
+        else:
+            print("song still playing")
+    inp = input("Enter Song Request with !song:")
+    req = inp[0:5]
+    if req == "!song":
+        search = inp[5:]
+        if len(search) == 0:
+            print("must add song request after !song")
+            continue
+        addqueu(search)
+        if not gotFirst:
+            first = True
+            gotFirst = True
+
+
